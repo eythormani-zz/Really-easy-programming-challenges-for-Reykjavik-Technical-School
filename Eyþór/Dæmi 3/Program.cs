46fdae9e94e366149f336756cc5ab6a3693c1fe0
Eyþór Máni Steinarsson <eythormani@gmail.com>
2015-10-14 23:13:00 +0000

    Bætti við því sem ég er kominn með so far
    Dæmi 1 til 4 eru að öllu tilbúin, annars eru hin meira concept

---
+++ Eyþór/Dæmi 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dæmi_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = null;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    b += char.ToUpper(a[i]);
+                }
+                else
+                {
+                    b += a[i];
+                }
+            }
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Eyþór/Dæmi 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dæmi_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = null;
+            for (int i = a.Length - 1; i != -1; i--)
+            {
+                b += a[i];
+            }
+            if (a == b)
+            {
+                Console.WriteLine("Strengurinn er eins speglaður!");
+            }
+            else
+            {
+                Console.WriteLine("Strengurinn speglast ekki");
+            }
+            Console.ReadLine();
+        }
+    }
+}
